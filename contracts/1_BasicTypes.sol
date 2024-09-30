b9bce67d670189080636a3632b4437855989f713
fabriziogianni7 <fabriziogiannitrapani@gmail.com>
2024-09-30 09:34:15 +0200

    functions and events

--- contracts/1_BasicTypes.sol
+++ contracts/1_BasicTypes.sol
@@ -5,7 +5,8 @@ pragma solidity >=0.8.2 <0.9.0;
 /**
  * this is a contract to learn data types and functions in solidity.
  * Create a contract that demonstrates the use of different data types (e.g., uint, int, bool, address, string, and arrays and mapping)
- * Create data type and setters
+ * Create data type, getters and setters
+ * at the end, you need to compile and deploy the contract using remix
  */
 
 
---
+++ contracts/2_Functions.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * this is a contract to learn functions, contract state and return values in solidity.
+ * Create a contract that contains 2 functions:
+    one has to modify the contract state and the other one has to return a value
+    one of them has to emit an event
+ * at the end, you need to compile and deploy the contract using remix
+ * check in the remix logs the event that you emitted
+ */
+
+
+contract Functions {
+
+    uint public unsignedInteger = 10;
+    event Hello(string indexed hello); // one function need to emit this event this way: emit Hello('hi')
+    
+    // assignment: create the functions here 
+
+}
