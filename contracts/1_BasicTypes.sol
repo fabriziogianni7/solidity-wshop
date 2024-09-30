c37dd59456f09c8604a16f1cbae4581bbc357ae1
fabriziogianni7 <fabriziogiannitrapani@gmail.com>
2024-09-30 09:25:02 +0200

    basic types

---
+++ contracts/1_BasicTypes.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * this is a contract to learn data types and functions in solidity.
+ * Create a contract that demonstrates the use of different data types (e.g., uint, int, bool, address, string, and arrays and mapping)
+ * Create data type and setters
+ */
+
+
+contract BasicTypes {
+
+    // uint : integers without sign:
+    uint public unsignedInteger = 10;
+    
+    function setUnsignedInteger(uint _value) public {
+        unsignedInteger = _value;
+    }
+
+    // assignment: do all the other data types  int, bool, address, string, and arrays and mapping
+}
